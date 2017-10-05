98f41d8f0f04e9b3e2dab9b7f3958378a39aca4e
Zexing Qu <zxqu@jlu.edu.cn>
2017-10-05 12:18:22 +0800

    2017-5-6

--- MSDFT/MSDFT_METHOD101.f90
+++ MSDFT/MSDFT_METHOD101.f90
@@ -8,7 +8,7 @@
 
       DH=DABS(HH11-HH22)
       DE=DABS(EXC1-EXC2)
-      A=DSQRT(DH**2+4.0D0*HH12)+DE-DH
+      A=DSQRT(DH**2+4.0D0*HH12**2)+DE-DH
       B=0.5D0*DSQRT(A**2-DE**2)
       IF(HH12>0.0D0) THEN
         EXC12=B-HH12
